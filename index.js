cc0d972dbd9d76d7cde4d0cb344837ab8060b08d
OgbuEsther <ogbuuzoma413@gmail.com>
2026-09-03 21:00:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const mongoose = require('mongoose');
+const userRoute = require("./routes/userRoutes.js")
+
+const compass_string = "mongodb://localhost:27017/cohort8_db"
+const atlas_string = "mongodb+srv://adinadev5_db_user:adinadev5_db_user@cluster0.k7dgx4z.mongodb.net/cohort8_db?appName=Cluster0"
+
+mongoose.connect(compass_string)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error("Connection Error: ", err));
+
+
+const app = express()
+const port = 5555
+
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("server is active")
+})
+app.use("/users", userRoute)
+app.listen(port, () => {
+    console.log(`server is up and running on port : ${port}`)
+})
